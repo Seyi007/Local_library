ebec97e0403f3131f1319902b50b5b1774cd6e76
Seyi <kuforiji98@gmail.com>
2023-03-29 12:21:37 +0100

    setting allowed host

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7241&*pt2msx7qxrm2_ki5s-#uzq&+821^u2#n(&k8qrb$-e61
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
